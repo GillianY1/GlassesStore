9eb72dab70ffc67296dfc31f02bd4a3315f14626
GillianY1 <gilliangilliany@gmail.com>
2021-07-28 00:01:57 +0800

    Update 2021-07-27T16:01:53.893Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,6 +2,14 @@
 
 console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  console.log('hello Gillian!');
+});
+$(function () {
+  var $li = $('ul.serial-list li');
+  $($li.eq(0).addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
+  $li.click(function () {
+    $($(this).find('a').attr('href')).show().siblings('.tab-inner').hide();
+    $(this).addClass('active').siblings('.active').removeClass('active');
+  });
 });
 //# sourceMappingURL=all.js.map
