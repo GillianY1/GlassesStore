bd340a403937d067f77ec981537add731a752a8b
GillianY1 <gilliangilliany@gmail.com>
2021-07-28 00:02:33 +0800

    add pagination module, add tab-switch

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,16 @@
 console.log('Hello!');
 
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+  console.log('hello Gillian!');
+});
+
+$(function(){
+  let $li= $('ul.serial-list li');
+  $($li.eq(0).addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
+  $li.click(
+    function(){
+      $($(this).find('a').attr('href')).show().siblings('.tab-inner').hide();
+      $(this).addClass('active').siblings('.active').removeClass('active');
+    }
+  );
 });
